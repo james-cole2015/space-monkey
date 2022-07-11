5270ec36e60e9a330726f675144e35d257fa6e76
Brooks <brooks@Brookss-MacBook-Pro.local>
2022-07-11 17:46:57 -0400

    adding ebs volume & attachment configuration

--- modules/ec2/output.tf
+++ modules/ec2/output.tf
@@ -1,3 +1,11 @@
 output "ec2_public_ip" {
   value = module.ec2_instance.public_ip
 }
+
+output "ec2_id" {
+  value = module.ec2_instance.id
+}
+
+output "ebs_vol_id {
+  value = aws_ebs_volume.ebs_vol_01
+}
