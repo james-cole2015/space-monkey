ae8efc71a351d9e86ecc675888882fc9c36a6414
Brooks <brooks@Brookss-MacBook-Pro.local>
2022-07-11 22:02:20 -0400

    moving data to new block

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -24,7 +24,7 @@ resource "aws_security_group" "allow_ssh" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${chomp(data.http.terraform_ip.body)}/32"]
   }
 
   egress {
@@ -50,7 +50,7 @@ resource "aws_security_group" "allow_http" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["${chomp(data.http.terraform_ip.body)}/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
