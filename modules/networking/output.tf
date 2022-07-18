1790cc8d4caa94d2eb33fb2d6658889cc6b71e10
brooks <brooks.houck@gmail.com>
2022-07-18 16:54:11 +0000

    finishing up sg rule test

--- modules/networking/output.tf
+++ modules/networking/output.tf
@@ -5,11 +5,11 @@ output "vpc" {
 output "allow_ssh" {
   value = aws_security_group.allow_ssh
 }
-
+/*
 output "allow_http" {
   value = aws_security_group.allow_http
 }
-
+*/
 output "allow_https" {
   value = aws_security_group.allow_https
 }
