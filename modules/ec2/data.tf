67821b4fe2a1e3b91c4053c80e2cdff23496f34f
brooks <brooks.houck@gmail.com>
2022-07-20 20:08:37 +0000

    changing filter

--- modules/ec2/data.tf
+++ modules/ec2/data.tf
@@ -1,4 +1,9 @@
 data "aws_ami" "ami" {
-    most_recent = true
-    owners = ["099720109477"]
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name = "architecture"
+    values = ["x86_64"]
+  }
 }
